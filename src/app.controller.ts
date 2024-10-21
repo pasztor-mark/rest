719c7087b78f5dc31a76997affa7e70446013588
Petrik Diak <diak@petrik.hu>
2024-10-21 12:34:25 +0200

    nem birom

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  @Render('index')
-  getHello() {
-    return {
-      message: this.appService.getHello()
-    };
+  products = [
+    "i1",
+    "i2",
+    "i3"
+  ]
+
+  @Get("products")
+  getProducts() {
+    return this.products
+  }
+  @Get("products/:id")
+  getProduct(@Param('id') id: string) {
+    return this.products[Number(id)]
   }
 }
+
